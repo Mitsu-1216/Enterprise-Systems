a158655013b5e5ea2fd4e46f77d49cc8043e928c
Mitsu-1216 <adieuer._.03.26@icloud.com>
2023-06-24 07:16:08 +0900

    modify

--- Project1/CodeFile1.cs
+++ Project1/CodeFile1.cs
@@ -4,6 +4,7 @@ using System.Windows.Forms;
 class CodeFile1 : Form
 {
     private Button[] bt = new Button[6];
+    private TableLayoutPanel tlp;
 
     public static void Main()
         Application.Run(new CodeFile1());
