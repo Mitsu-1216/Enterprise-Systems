4b0d531ffc4b055b47e7c09ae2772878da9ccacf
Mitsu-1216 <adieuer._.03.26@icloud.com>
2023-06-23 22:01:43 +0900

    main

--- Project1/CodeFile1.cs
+++ Project1/CodeFile1.cs
@@ -3,8 +3,14 @@ using System.Windows.Forms;
 
 class CodeFile1 : Form
 {
+    private Button[] bt = new Button[6];
 
-    public static void main()
+    public static void Main()
         Application.Run(new Codfile1());
+
+        public Codefile1()
+    {
+
+    }
 
 }
