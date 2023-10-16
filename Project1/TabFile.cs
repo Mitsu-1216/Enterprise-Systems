a067b5363df2d8a165cd1bbea772054c026421f9
Mitsu-1216 <adieuer._.03.26@icloud.com>
2023-10-16 23:10:42 +0900

    TabControl

--- Project1/TabFile.cs
+++ Project1/TabFile.cs
@@ -4,13 +4,15 @@ using System.Windows.Forms;
 class TabFile : Form
 {
     private Button[] bt = new Button[6];
-    private Button button1;
-    private Label label1;
-    private Label label2;
-    private TextBox textBox1;
-    private TextBox textBox2;
-    private TableLayoutPanel tableLayoutPanel1;
     private Panel panel1;
+    private TabControl tabControl1;
+    private TabPage tabPage1;
+    private TabPage tabPage2;
+    private TabPage tabPage3;
+    private TabPage tabPage4;
+    private TabPage tabPage5;
+    private TabPage tabPage6;
+    private TabPage tabPage7;
     private TableLayoutPanel tlp;
 
     public TabFile()
@@ -24,105 +26,122 @@ class TabFile : Form
 
     private void InitializeComponent()
     {
-        this.button1 = new System.Windows.Forms.Button();
-        this.label1 = new System.Windows.Forms.Label();
-        this.label2 = new System.Windows.Forms.Label();
-        this.textBox1 = new System.Windows.Forms.TextBox();
-        this.textBox2 = new System.Windows.Forms.TextBox();
-        this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
-        this.panel1 = new System.Windows.Forms.Panel();
-        this.tableLayoutPanel1.SuspendLayout();
-        this.panel1.SuspendLayout();
-        this.SuspendLayout();
-        // 
-        // button1
-        // 
-        this.button1.Location = new System.Drawing.Point(343, 423);
-        this.button1.Name = "button1";
-        this.button1.Size = new System.Drawing.Size(75, 23);
-        this.button1.TabIndex = 0;
-        this.button1.Text = "Login";
-        this.button1.UseVisualStyleBackColor = true;
-        this.button1.Click += new System.EventHandler(this.button1_Click);
-        // 
-        // label1
-        // 
-        this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
-        this.label1.AutoSize = true;
-        this.label1.Location = new System.Drawing.Point(94, 0);
-        this.label1.Name = "label1";
-        this.label1.Size = new System.Drawing.Size(43, 132);
-        this.label1.TabIndex = 1;
-        this.label1.Text = "LoginID";
-        this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-        // 
-        // label2
-        // 
-        this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
-        this.label2.AutoSize = true;
-        this.label2.Location = new System.Drawing.Point(88, 132);
-        this.label2.Name = "label2";
-        this.label2.Size = new System.Drawing.Size(55, 133);
-        this.label2.TabIndex = 2;
-        this.label2.Text = "PassWord";
-        this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-        // 
-        // textBox1
-        // 
-        this.textBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
-        this.textBox1.Location = new System.Drawing.Point(234, 56);
-        this.textBox1.Name = "textBox1";
-        this.textBox1.RightToLeft = System.Windows.Forms.RightToLeft.No;
-        this.textBox1.Size = new System.Drawing.Size(225, 19);
-        this.textBox1.TabIndex = 3;
-        this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
-        this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
-        // 
-        // textBox2
-        // 
-        this.textBox2.Anchor = System.Windows.Forms.AnchorStyles.None;
-        this.textBox2.Location = new System.Drawing.Point(234, 189);
-        this.textBox2.Name = "textBox2";
-        this.textBox2.Size = new System.Drawing.Size(225, 19);
-        this.textBox2.TabIndex = 4;
-        // 
-        // tableLayoutPanel1
-        // 
-        this.tableLayoutPanel1.ColumnCount = 2;
-        this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-        this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-        this.tableLayoutPanel1.Controls.Add(this.label1, 1, 0);
-        this.tableLayoutPanel1.Controls.Add(this.textBox2, 0, 1);
-        this.tableLayoutPanel1.Controls.Add(this.label2, 1, 1);
-        this.tableLayoutPanel1.Controls.Add(this.textBox1, 0, 0);
-        this.tableLayoutPanel1.Location = new System.Drawing.Point(144, 114);
-        this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-        this.tableLayoutPanel1.RowCount = 2;
-        this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-        this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-        this.tableLayoutPanel1.Size = new System.Drawing.Size(462, 265);
-        this.tableLayoutPanel1.TabIndex = 5;
-        // 
-        // panel1
-        // 
-        this.panel1.Controls.Add(this.button1);
-        this.panel1.Controls.Add(this.tableLayoutPanel1);
-        this.panel1.Cursor = System.Windows.Forms.Cursors.Hand;
-        this.panel1.Location = new System.Drawing.Point(0, 0);
-        this.panel1.Name = "panel1";
-        this.panel1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-        this.panel1.Size = new System.Drawing.Size(788, 594);
-        this.panel1.TabIndex = 6;
-        // 
-        // CodeFile1
-        // 
-        this.ClientSize = new System.Drawing.Size(786, 513);
-        this.Controls.Add(this.panel1);
-        this.Name = "CodeFile1";
-        this.tableLayoutPanel1.ResumeLayout(false);
-        this.tableLayoutPanel1.PerformLayout();
-        this.panel1.ResumeLayout(false);
-        this.ResumeLayout(false);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.tabPage5 = new System.Windows.Forms.TabPage();
+            this.tabPage6 = new System.Windows.Forms.TabPage();
+            this.tabPage7 = new System.Windows.Forms.TabPage();
+            this.panel1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.tabControl1);
+            this.panel1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.panel1.Size = new System.Drawing.Size(788, 594);
+            this.panel1.TabIndex = 6;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Controls.Add(this.tabPage4);
+            this.tabControl1.Controls.Add(this.tabPage5);
+            this.tabControl1.Controls.Add(this.tabPage6);
+            this.tabControl1.Controls.Add(this.tabPage7);
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(785, 515);
+            this.tabControl1.TabIndex = 7;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(777, 489);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(777, 489);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(777, 489);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "tabPage3";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Location = new System.Drawing.Point(4, 22);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage4.Size = new System.Drawing.Size(777, 489);
+            this.tabPage4.TabIndex = 3;
+            this.tabPage4.Text = "tabPage4";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // tabPage5
+            // 
+            this.tabPage5.Location = new System.Drawing.Point(4, 22);
+            this.tabPage5.Name = "tabPage5";
+            this.tabPage5.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage5.Size = new System.Drawing.Size(777, 489);
+            this.tabPage5.TabIndex = 4;
+            this.tabPage5.Text = "tabPage5";
+            this.tabPage5.UseVisualStyleBackColor = true;
+            // 
+            // tabPage6
+            // 
+            this.tabPage6.Location = new System.Drawing.Point(4, 22);
+            this.tabPage6.Name = "tabPage6";
+            this.tabPage6.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage6.Size = new System.Drawing.Size(777, 489);
+            this.tabPage6.TabIndex = 5;
+            this.tabPage6.Text = "tabPage6";
+            this.tabPage6.UseVisualStyleBackColor = true;
+            // 
+            // tabPage7
+            // 
+            this.tabPage7.Location = new System.Drawing.Point(4, 22);
+            this.tabPage7.Name = "tabPage7";
+            this.tabPage7.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage7.Size = new System.Drawing.Size(777, 489);
+            this.tabPage7.TabIndex = 6;
+            this.tabPage7.Text = "tabPage7";
+            this.tabPage7.UseVisualStyleBackColor = true;
+            // 
+            // TabFile
+            // 
+            this.ClientSize = new System.Drawing.Size(786, 513);
+            this.Controls.Add(this.panel1);
+            this.Name = "TabFile";
+            this.panel1.ResumeLayout(false);
+            this.tabControl1.ResumeLayout(false);
+            this.ResumeLayout(false);
 
     }
 
