870d3c18047d6b092e24b0ca86833bf2e523f658
Mitsu-1216 <adieuer._.03.26@icloud.com>
2023-10-17 22:26:30 +0900

    tabname

--- Project1/TabFile.cs
+++ Project1/TabFile.cs
@@ -6,8 +6,8 @@ class TabFile : Form
     private Button[] bt = new Button[6];
     private Panel panel1;
     private TabControl tabControl1;
-    private TabPage tabPage1;
-    private TabPage tabPage2;
+    private TabPage 顧客基本情報;
+    private TabPage 購入履歴;
     private TabPage tabPage3;
     private TabPage tabPage4;
     private TabPage tabPage5;
@@ -28,8 +28,8 @@ class TabFile : Form
     {
             this.panel1 = new System.Windows.Forms.Panel();
             this.tabControl1 = new System.Windows.Forms.TabControl();
-            this.tabPage1 = new System.Windows.Forms.TabPage();
-            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.顧客基本情報 = new System.Windows.Forms.TabPage();
+            this.購入履歴 = new System.Windows.Forms.TabPage();
             this.tabPage3 = new System.Windows.Forms.TabPage();
             this.tabPage4 = new System.Windows.Forms.TabPage();
             this.tabPage5 = new System.Windows.Forms.TabPage();
@@ -51,8 +51,8 @@ class TabFile : Form
             // 
             // tabControl1
             // 
-            this.tabControl1.Controls.Add(this.tabPage1);
-            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.顧客基本情報);
+            this.tabControl1.Controls.Add(this.購入履歴);
             this.tabControl1.Controls.Add(this.tabPage3);
             this.tabControl1.Controls.Add(this.tabPage4);
             this.tabControl1.Controls.Add(this.tabPage5);
@@ -64,26 +64,27 @@ class TabFile : Form
             this.tabControl1.Size = new System.Drawing.Size(785, 515);
             this.tabControl1.TabIndex = 7;
             // 
-            // tabPage1
+            // 顧客基本情報
             // 
-            this.tabPage1.Location = new System.Drawing.Point(4, 22);
-            this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(777, 489);
-            this.tabPage1.TabIndex = 0;
-            this.tabPage1.Text = "tabPage1";
-            this.tabPage1.UseVisualStyleBackColor = true;
+            this.顧客基本情報.BackColor = System.Drawing.Color.FloralWhite;
+            this.顧客基本情報.Location = new System.Drawing.Point(4, 22);
+            this.顧客基本情報.Name = "顧客基本情報";
+            this.顧客基本情報.Padding = new System.Windows.Forms.Padding(3);
+            this.顧客基本情報.Size = new System.Drawing.Size(777, 489);
+            this.顧客基本情報.TabIndex = 0;
+            this.顧客基本情報.Text = "顧客基本情報";
+            this.顧客基本情報.ToolTipText = "顧客の基本情報を表示します。";
             // 
-            // tabPage2
+            // 購入履歴
             // 
-            this.tabPage2.Location = new System.Drawing.Point(4, 22);
-            this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(777, 489);
-            this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "tabPage2";
-            this.tabPage2.UseVisualStyleBackColor = true;
-            // 
+            this.購入履歴.Location = new System.Drawing.Point(4, 22);
+            this.購入履歴.Name = "tabPage2";
+            this.購入履歴.Padding = new System.Windows.Forms.Padding(3);
+            this.購入履歴.Size = new System.Drawing.Size(777, 489);
+            this.購入履歴.TabIndex = 1;
+            this.購入履歴.Text = "購入履歴";
+            this.購入履歴.UseVisualStyleBackColor = true;
+            //  
             // tabPage3
             // 
             this.tabPage3.Location = new System.Drawing.Point(4, 22);
