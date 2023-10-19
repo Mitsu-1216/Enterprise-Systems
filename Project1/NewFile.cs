dd6b8a5f70fe472ade868c779a1e874c9ada0e72
Mitsu-1216 <adieuer._.03.26@icloud.com>
2023-10-19 23:54:07 +0900

    新規登録画面レイアウト

--- Project1/NewFile.cs
+++ Project1/NewFile.cs
@@ -75,16 +75,16 @@ class NewFile : Form
             this.label9 = new System.Windows.Forms.Label();
             this.label10 = new System.Windows.Forms.Label();
             this.label11 = new System.Windows.Forms.Label();
-            this.button2 = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
             this.label12 = new System.Windows.Forms.Label();
             this.label13 = new System.Windows.Forms.Label();
-            this.textBox7 = new System.Windows.Forms.TextBox();
             this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
             this.textBox9 = new System.Windows.Forms.TextBox();
             this.textBox10 = new System.Windows.Forms.TextBox();
             this.textBox11 = new System.Windows.Forms.TextBox();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.tableLayoutPanel1.SuspendLayout();
             this.tableLayoutPanel2.SuspendLayout();
@@ -172,8 +172,9 @@ class NewFile : Form
             // 
             // label7
             // 
+            this.label7.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(409, 125);
+            this.label7.Location = new System.Drawing.Point(206, 129);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(29, 12);
             this.label7.TabIndex = 12;
@@ -181,8 +182,9 @@ class NewFile : Form
             // 
             // label6
             // 
+            this.label6.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(385, 105);
+            this.label6.Location = new System.Drawing.Point(194, 109);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(53, 12);
             this.label6.TabIndex = 11;
@@ -190,8 +192,9 @@ class NewFile : Form
             // 
             // label5
             // 
+            this.label5.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(385, 80);
+            this.label5.Location = new System.Drawing.Point(194, 86);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(53, 12);
             this.label5.TabIndex = 10;
@@ -199,8 +202,9 @@ class NewFile : Form
             // 
             // label4
             // 
+            this.label4.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(409, 60);
+            this.label4.Location = new System.Drawing.Point(206, 64);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(29, 12);
             this.label4.TabIndex = 9;
@@ -208,8 +212,9 @@ class NewFile : Form
             // 
             // label3
             // 
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(398, 30);
+            this.label3.Location = new System.Drawing.Point(201, 39);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(40, 12);
             this.label3.TabIndex = 8;
@@ -217,12 +222,13 @@ class NewFile : Form
             // 
             // tableLayoutPanel2
             // 
+            this.tableLayoutPanel2.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.tableLayoutPanel2.ColumnCount = 2;
             this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.tableLayoutPanel2.Controls.Add(this.textBox2, 0, 0);
             this.tableLayoutPanel2.Controls.Add(this.textBox1, 1, 0);
-            this.tableLayoutPanel2.Location = new System.Drawing.Point(785, 3);
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(615, 3);
             this.tableLayoutPanel2.Name = "tableLayoutPanel2";
             this.tableLayoutPanel2.RowCount = 1;
             this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
@@ -248,19 +254,21 @@ class NewFile : Form
             // 
             // dateTimePicker1
             // 
-            this.dateTimePicker1.Location = new System.Drawing.Point(785, 83);
+            this.dateTimePicker1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.dateTimePicker1.Location = new System.Drawing.Point(615, 83);
             this.dateTimePicker1.Name = "dateTimePicker1";
             this.dateTimePicker1.Size = new System.Drawing.Size(94, 19);
             this.dateTimePicker1.TabIndex = 2;
             // 
             // tableLayoutPanel4
             // 
+            this.tableLayoutPanel4.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.tableLayoutPanel4.ColumnCount = 2;
             this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.tableLayoutPanel4.Controls.Add(this.radioButton1, 1, 0);
             this.tableLayoutPanel4.Controls.Add(this.radioButton2, 0, 0);
-            this.tableLayoutPanel4.Location = new System.Drawing.Point(785, 63);
+            this.tableLayoutPanel4.Location = new System.Drawing.Point(615, 63);
             this.tableLayoutPanel4.Name = "tableLayoutPanel4";
             this.tableLayoutPanel4.RowCount = 1;
             this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
@@ -292,12 +300,13 @@ class NewFile : Form
             // 
             // tableLayoutPanel3
             // 
+            this.tableLayoutPanel3.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.tableLayoutPanel3.ColumnCount = 2;
             this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.tableLayoutPanel3.Controls.Add(this.textBox3, 0, 0);
             this.tableLayoutPanel3.Controls.Add(this.textBox4, 1, 0);
-            this.tableLayoutPanel3.Location = new System.Drawing.Point(785, 33);
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(615, 33);
             this.tableLayoutPanel3.Name = "tableLayoutPanel3";
             this.tableLayoutPanel3.RowCount = 1;
             this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
@@ -323,8 +332,9 @@ class NewFile : Form
             // 
             // label2
             // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(399, 0);
+            this.label2.Location = new System.Drawing.Point(201, 9);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(39, 12);
             this.label2.TabIndex = 7;
@@ -332,8 +342,9 @@ class NewFile : Form
             // 
             // label8
             // 
+            this.label8.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(385, 145);
+            this.label8.Location = new System.Drawing.Point(194, 149);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(53, 12);
             this.label8.TabIndex = 15;
@@ -341,8 +352,9 @@ class NewFile : Form
             // 
             // label9
             // 
+            this.label9.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(369, 165);
+            this.label9.Location = new System.Drawing.Point(186, 169);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(69, 12);
             this.label9.TabIndex = 16;
@@ -350,8 +362,9 @@ class NewFile : Form
             // 
             // label10
             // 
+            this.label10.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.label10.AutoSize = true;
-            this.label10.Location = new System.Drawing.Point(409, 185);
+            this.label10.Location = new System.Drawing.Point(206, 189);
             this.label10.Name = "label10";
             this.label10.Size = new System.Drawing.Size(29, 12);
             this.label10.TabIndex = 17;
@@ -359,36 +372,19 @@ class NewFile : Form
             // 
             // label11
             // 
+            this.label11.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.label11.AutoSize = true;
-            this.label11.Location = new System.Drawing.Point(397, 205);
+            this.label11.Location = new System.Drawing.Point(200, 209);
             this.label11.Name = "label11";
             this.label11.Size = new System.Drawing.Size(41, 12);
             this.label11.TabIndex = 18;
             this.label11.Text = "出身地";
             // 
-            // button2
-            // 
-            this.button2.Font = new System.Drawing.Font("BIZ UDPゴシック", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.button2.Location = new System.Drawing.Point(393, 599);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(143, 45);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "登録する";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(23, 20);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(138, 12);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "新規顧客情報を登録します";
-            // 
             // label12
             // 
+            this.label12.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.label12.AutoSize = true;
-            this.label12.Location = new System.Drawing.Point(409, 225);
+            this.label12.Location = new System.Drawing.Point(206, 229);
             this.label12.Name = "label12";
             this.label12.Size = new System.Drawing.Size(29, 12);
             this.label12.TabIndex = 19;
@@ -396,20 +392,13 @@ class NewFile : Form
             // 
             // label13
             // 
+            this.label13.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.label13.AutoSize = true;
-            this.label13.Location = new System.Drawing.Point(416, 245);
+            this.label13.Location = new System.Drawing.Point(210, 384);
             this.label13.Name = "label13";
             this.label13.Size = new System.Drawing.Size(22, 12);
             this.label13.TabIndex = 20;
             this.label13.Text = "メモ";
-            // 
-            // textBox7
-            // 
-            this.textBox7.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.textBox7.Location = new System.Drawing.Point(641, 148);
-            this.textBox7.Name = "textBox7";
-            this.textBox7.Size = new System.Drawing.Size(41, 19);
-            this.textBox7.TabIndex = 21;
             // 
             // textBox8
             // 
@@ -418,6 +407,14 @@ class NewFile : Form
             this.textBox8.Name = "textBox8";
             this.textBox8.Size = new System.Drawing.Size(41, 19);
             this.textBox8.TabIndex = 22;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.textBox7.Location = new System.Drawing.Point(641, 148);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(41, 19);
+            this.textBox7.TabIndex = 21;
             // 
             // textBox9
             // 
@@ -445,11 +442,31 @@ class NewFile : Form
             // 
             // comboBox1
             // 
+            this.comboBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(758, 208);
+            this.comboBox1.Location = new System.Drawing.Point(601, 208);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(121, 20);
             this.comboBox1.TabIndex = 26;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("BIZ UDPゴシック", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button2.Location = new System.Drawing.Point(393, 599);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(143, 45);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "登録する";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(138, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "新規顧客情報を登録します";
             // 
             // NewFile
             // 
@@ -484,8 +501,4 @@ class NewFile : Form
         }
     }
 
-    private void label11_Click(object sender, EventArgs e)
-    {
-
-    }
 }
