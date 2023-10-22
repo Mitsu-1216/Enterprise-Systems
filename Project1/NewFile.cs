bc3f12e17b8068d3ce6dccd279f1a7a7bdeb9090
Mitsu-1216 <adieuer._.03.26@icloud.com>
2023-10-22 12:43:10 +0900

    レイアウト変更

--- Project1/NewFile.cs
+++ Project1/NewFile.cs
@@ -472,10 +472,10 @@ class NewFile : Form
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("BIZ UDPゴシック", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Font = new System.Drawing.Font("BIZ UDPゴシック", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
             this.label1.Location = new System.Drawing.Point(23, 20);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(145, 12);
+            this.label1.Size = new System.Drawing.Size(195, 16);
             this.label1.TabIndex = 1;
             this.label1.Text = "新規顧客情報を登録します";
             // 
--- Project1/TabFile.cs
+++ Project1/TabFile.cs
@@ -77,6 +77,7 @@ class TabFile : Form
     private RadioButton radioButton2;
     private Button button2;
     private Label label1;
+    private Button button1;
     private TableLayoutPanel tlp;
 
     public TabFile()
@@ -164,6 +165,7 @@ class TabFile : Form
             this.radioButton2 = new System.Windows.Forms.RadioButton();
             this.button2 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
@@ -887,9 +889,20 @@ class TabFile : Form
             this.label1.TabIndex = 1;
             this.label1.Text = "新規顧客情報を登録します";
             // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("BIZ UDPゴシック", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button1.Location = new System.Drawing.Point(1017, 444);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(106, 59);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "編集する\r\n";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // TabFile
             // 
             this.ClientSize = new System.Drawing.Size(1484, 861);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.panel1);
             this.Name = "TabFile";
             this.Text = "顧客情報詳細画面";
