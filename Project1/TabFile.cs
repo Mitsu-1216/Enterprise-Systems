dcdd98b7077bd65851ebe11f404379297e108e90
Mitsu-1216 <adieuer._.03.26@icloud.com>
2023-11-04 20:56:30 +0900

    購入履歴データグリッド

--- Project1/TabFile.cs
+++ Project1/TabFile.cs
@@ -22,7 +22,7 @@ class TabFile : Form
     private Panel panel2;
     private Label label1;
     private TabPage tabPage2;
-    private DataGridView dataGridView1;
+    private DataGridView dataGridView_purchaseinfo;
     private TabPage tabPage3;
     private TabPage tabPage4;
     private TabPage tabPage5;
@@ -76,11 +76,6 @@ class TabFile : Form
     private RadioButton radioButtonWoman;
     private ComboBox comboBoxBirthPlace;
     private Button edit_button;
-    private DataGridViewTextBoxColumn 購入日;
-    private DataGridViewTextBoxColumn 支払方法;
-    private DataGridViewTextBoxColumn 担当者;
-    private DataGridViewTextBoxColumn 金額;
-    private DataGridViewTextBoxColumn 購入内容;
     private int customerId;
 
     public TabFile(DataTable customerData, DataTable dt_purchase)
@@ -147,6 +142,9 @@ class TabFile : Form
             }
         }
 
+        dataGridView_purchaseinfo.DataSource = dt_purchase;
+
+
     }
 
 
@@ -198,7 +196,7 @@ class TabFile : Form
             this.comboBoxBirthPlace = new System.Windows.Forms.ComboBox();
             this.label1 = new System.Windows.Forms.Label();
             this.tabPage2 = new System.Windows.Forms.TabPage();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView_purchaseinfo = new System.Windows.Forms.DataGridView();
             this.tabPage3 = new System.Windows.Forms.TabPage();
             this.tabPage4 = new System.Windows.Forms.TabPage();
             this.tabPage5 = new System.Windows.Forms.TabPage();
@@ -221,11 +219,6 @@ class TabFile : Form
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.購入日 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.支払方法 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.担当者 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.金額 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.購入内容 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
@@ -237,7 +230,7 @@ class TabFile : Form
             this.tableLayoutPanel3.SuspendLayout();
             this.tableLayoutPanel4.SuspendLayout();
             this.tabPage2.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_purchaseinfo)).BeginInit();
             this.tabPage7.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.bindingNavigator1)).BeginInit();
             this.bindingNavigator1.SuspendLayout();
@@ -698,7 +691,7 @@ class TabFile : Form
             // 
             // tabPage2
             // 
-            this.tabPage2.Controls.Add(this.dataGridView1);
+            this.tabPage2.Controls.Add(this.dataGridView_purchaseinfo);
             this.tabPage2.Location = new System.Drawing.Point(4, 22);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
@@ -707,21 +700,15 @@ class TabFile : Form
             this.tabPage2.Text = "購入履歴";
             this.tabPage2.UseVisualStyleBackColor = true;
             // 
-            // dataGridView1
+            // dataGridView_purchaseinfo
             // 
-            this.dataGridView1.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.購入日,
-            this.支払方法,
-            this.担当者,
-            this.金額,
-            this.購入内容});
-            this.dataGridView1.Location = new System.Drawing.Point(34, 46);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowTemplate.Height = 21;
-            this.dataGridView1.Size = new System.Drawing.Size(1080, 489);
-            this.dataGridView1.TabIndex = 0;
+            this.dataGridView_purchaseinfo.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.dataGridView_purchaseinfo.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView_purchaseinfo.Location = new System.Drawing.Point(34, 46);
+            this.dataGridView_purchaseinfo.Name = "dataGridView_purchaseinfo";
+            this.dataGridView_purchaseinfo.RowTemplate.Height = 21;
+            this.dataGridView_purchaseinfo.Size = new System.Drawing.Size(1080, 489);
+            this.dataGridView_purchaseinfo.TabIndex = 0;
             // 
             // tabPage3
             // 
@@ -933,31 +920,6 @@ class TabFile : Form
             this.panel1.Size = new System.Drawing.Size(1472, 809);
             this.panel1.TabIndex = 6;
             // 
-            // 購入日
-            // 
-            this.購入日.HeaderText = "購入日";
-            this.購入日.Name = "購入日";
-            // 
-            // 支払方法
-            // 
-            this.支払方法.HeaderText = "支払方法";
-            this.支払方法.Name = "支払方法";
-            // 
-            // 担当者
-            // 
-            this.担当者.HeaderText = "担当者";
-            this.担当者.Name = "担当者";
-            // 
-            // 金額
-            // 
-            this.金額.HeaderText = "金額";
-            this.金額.Name = "金額";
-            // 
-            // 購入内容
-            // 
-            this.購入内容.HeaderText = "購入内容";
-            this.購入内容.Name = "購入内容";
-            // 
             // TabFile
             // 
             this.ClientSize = new System.Drawing.Size(1484, 861);
@@ -981,7 +943,7 @@ class TabFile : Form
             this.tableLayoutPanel4.ResumeLayout(false);
             this.tableLayoutPanel4.PerformLayout();
             this.tabPage2.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_purchaseinfo)).EndInit();
             this.tabPage7.ResumeLayout(false);
             this.tabPage7.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.bindingNavigator1)).EndInit();
