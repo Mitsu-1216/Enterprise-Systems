fca7881e36b7c2316ecb1ba377b15c80fe8bb06e
Mitsu-1216 <adieuer._.03.26@icloud.com>
2023-11-05 20:51:54 +0900

    支払方法ラジオボタン

--- Project1/TabFile.cs
+++ Project1/TabFile.cs
@@ -104,6 +104,8 @@ class TabFile : Form
     private ComboBox comboBox2;
     private TextBox textBox4;
     private Label label19;
+    private RadioButton radioButton9;
+    private RadioButton radioButton10;
     private int customerId;
 
     public TabFile(DataTable customerData, DataTable dt_purchase)
@@ -280,6 +282,8 @@ class TabFile : Form
             this.comboBox3 = new System.Windows.Forms.ComboBox();
             this.label22 = new System.Windows.Forms.Label();
             this.textBox5 = new System.Windows.Forms.TextBox();
+            this.radioButton9 = new System.Windows.Forms.RadioButton();
+            this.radioButton10 = new System.Windows.Forms.RadioButton();
             ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
@@ -1016,7 +1020,7 @@ class TabFile : Form
             this.tableLayoutPanel5.Controls.Add(this.dateTimePicker2, 0, 0);
             this.tableLayoutPanel5.Controls.Add(this.comboBox1, 0, 2);
             this.tableLayoutPanel5.Controls.Add(this.textBox3, 0, 3);
-            this.tableLayoutPanel5.Location = new System.Drawing.Point(28, 35);
+            this.tableLayoutPanel5.Location = new System.Drawing.Point(28, 57);
             this.tableLayoutPanel5.Name = "tableLayoutPanel5";
             this.tableLayoutPanel5.RowCount = 4;
             this.tableLayoutPanel5.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
@@ -1072,7 +1076,7 @@ class TabFile : Form
             // 
             this.label21.AutoSize = true;
             this.label21.Font = new System.Drawing.Font("BIZ UDPゴシック", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.label21.Location = new System.Drawing.Point(16, 3);
+            this.label21.Location = new System.Drawing.Point(15, 16);
             this.label21.Name = "label21";
             this.label21.RightToLeft = System.Windows.Forms.RightToLeft.No;
             this.label21.Size = new System.Drawing.Size(173, 16);
@@ -1092,21 +1096,21 @@ class TabFile : Form
             this.radioButton1.AutoSize = true;
             this.radioButton1.Location = new System.Drawing.Point(3, 3);
             this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(110, 16);
+            this.radioButton1.Size = new System.Drawing.Size(47, 16);
             this.radioButton1.TabIndex = 6;
             this.radioButton1.TabStop = true;
-            this.radioButton1.Text = "radioButton1";
+            this.radioButton1.Text = "現金";
             this.radioButton1.UseVisualStyleBackColor = true;
             // 
             // radioButton2
             // 
             this.radioButton2.AutoSize = true;
-            this.radioButton2.Location = new System.Drawing.Point(119, 3);
+            this.radioButton2.Location = new System.Drawing.Point(56, 3);
             this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(111, 16);
+            this.radioButton2.Size = new System.Drawing.Size(106, 16);
             this.radioButton2.TabIndex = 7;
             this.radioButton2.TabStop = true;
-            this.radioButton2.Text = "radioButton2";
+            this.radioButton2.Text = "クレジットカード";
             this.radioButton2.UseVisualStyleBackColor = true;
             // 
             // flowLayoutPanel3
@@ -1119,6 +1123,8 @@ class TabFile : Form
             this.flowLayoutPanel3.Controls.Add(this.radioButton6);
             this.flowLayoutPanel3.Controls.Add(this.radioButton7);
             this.flowLayoutPanel3.Controls.Add(this.radioButton8);
+            this.flowLayoutPanel3.Controls.Add(this.radioButton9);
+            this.flowLayoutPanel3.Controls.Add(this.radioButton10);
             this.flowLayoutPanel3.Location = new System.Drawing.Point(126, 54);
             this.flowLayoutPanel3.Name = "flowLayoutPanel3";
             this.flowLayoutPanel3.RightToLeft = System.Windows.Forms.RightToLeft.No;
@@ -1128,67 +1134,67 @@ class TabFile : Form
             // radioButton3
             // 
             this.radioButton3.AutoSize = true;
-            this.radioButton3.Location = new System.Drawing.Point(236, 3);
+            this.radioButton3.Location = new System.Drawing.Point(168, 3);
             this.radioButton3.Name = "radioButton3";
-            this.radioButton3.Size = new System.Drawing.Size(111, 16);
+            this.radioButton3.Size = new System.Drawing.Size(83, 16);
             this.radioButton3.TabIndex = 9;
             this.radioButton3.TabStop = true;
-            this.radioButton3.Text = "radioButton3";
+            this.radioButton3.Text = "QUICPay";
             this.radioButton3.UseVisualStyleBackColor = true;
             // 
             // radioButton4
             // 
             this.radioButton4.AutoSize = true;
-            this.radioButton4.Location = new System.Drawing.Point(353, 3);
+            this.radioButton4.Location = new System.Drawing.Point(257, 3);
             this.radioButton4.Name = "radioButton4";
-            this.radioButton4.Size = new System.Drawing.Size(111, 16);
+            this.radioButton4.Size = new System.Drawing.Size(71, 16);
             this.radioButton4.TabIndex = 10;
             this.radioButton4.TabStop = true;
-            this.radioButton4.Text = "radioButton4";
+            this.radioButton4.Text = "PayPay";
             this.radioButton4.UseVisualStyleBackColor = true;
             // 
             // radioButton5
             // 
             this.radioButton5.AutoSize = true;
-            this.radioButton5.Location = new System.Drawing.Point(3, 25);
+            this.radioButton5.Location = new System.Drawing.Point(334, 3);
             this.radioButton5.Name = "radioButton5";
-            this.radioButton5.Size = new System.Drawing.Size(111, 16);
+            this.radioButton5.Size = new System.Drawing.Size(68, 16);
             this.radioButton5.TabIndex = 10;
             this.radioButton5.TabStop = true;
-            this.radioButton5.Text = "radioButton5";
+            this.radioButton5.Text = "楽天ペイ";
             this.radioButton5.UseVisualStyleBackColor = true;
             // 
             // radioButton6
             // 
             this.radioButton6.AutoSize = true;
-            this.radioButton6.Location = new System.Drawing.Point(120, 25);
+            this.radioButton6.Location = new System.Drawing.Point(3, 25);
             this.radioButton6.Name = "radioButton6";
-            this.radioButton6.Size = new System.Drawing.Size(111, 16);
+            this.radioButton6.Size = new System.Drawing.Size(70, 16);
             this.radioButton6.TabIndex = 11;
             this.radioButton6.TabStop = true;
-            this.radioButton6.Text = "radioButton6";
+            this.radioButton6.Text = "au PAY";
             this.radioButton6.UseVisualStyleBackColor = true;
             // 
             // radioButton7
             // 
             this.radioButton7.AutoSize = true;
-            this.radioButton7.Location = new System.Drawing.Point(237, 25);
+            this.radioButton7.Location = new System.Drawing.Point(79, 25);
             this.radioButton7.Name = "radioButton7";
-            this.radioButton7.Size = new System.Drawing.Size(111, 16);
+            this.radioButton7.Size = new System.Drawing.Size(81, 16);
             this.radioButton7.TabIndex = 12;
             this.radioButton7.TabStop = true;
-            this.radioButton7.Text = "radioButton7";
+            this.radioButton7.Text = "LINE Pay";
             this.radioButton7.UseVisualStyleBackColor = true;
             // 
             // radioButton8
             // 
             this.radioButton8.AutoSize = true;
-            this.radioButton8.Location = new System.Drawing.Point(354, 25);
+            this.radioButton8.Location = new System.Drawing.Point(166, 25);
             this.radioButton8.Name = "radioButton8";
-            this.radioButton8.Size = new System.Drawing.Size(111, 16);
+            this.radioButton8.Size = new System.Drawing.Size(78, 16);
             this.radioButton8.TabIndex = 13;
             this.radioButton8.TabStop = true;
-            this.radioButton8.Text = "radioButton8";
+            this.radioButton8.Text = "FamiPay";
             this.radioButton8.UseVisualStyleBackColor = true;
             // 
             // comboBox1
@@ -1227,7 +1233,7 @@ class TabFile : Form
             this.tableLayoutPanel6.Controls.Add(this.comboBox2, 5, 0);
             this.tableLayoutPanel6.Controls.Add(this.textBox4, 3, 0);
             this.tableLayoutPanel6.Controls.Add(this.label19, 4, 0);
-            this.tableLayoutPanel6.Location = new System.Drawing.Point(28, 247);
+            this.tableLayoutPanel6.Location = new System.Drawing.Point(28, 269);
             this.tableLayoutPanel6.Name = "tableLayoutPanel6";
             this.tableLayoutPanel6.RightToLeft = System.Windows.Forms.RightToLeft.No;
             this.tableLayoutPanel6.RowCount = 1;
@@ -1308,6 +1314,28 @@ class TabFile : Form
             this.textBox5.Name = "textBox5";
             this.textBox5.Size = new System.Drawing.Size(89, 19);
             this.textBox5.TabIndex = 12;
+            // 
+            // radioButton9
+            // 
+            this.radioButton9.AutoSize = true;
+            this.radioButton9.Location = new System.Drawing.Point(250, 25);
+            this.radioButton9.Name = "radioButton9";
+            this.radioButton9.Size = new System.Drawing.Size(59, 16);
+            this.radioButton9.TabIndex = 14;
+            this.radioButton9.TabStop = true;
+            this.radioButton9.Text = "後払い";
+            this.radioButton9.UseVisualStyleBackColor = true;
+            // 
+            // radioButton10
+            // 
+            this.radioButton10.AutoSize = true;
+            this.radioButton10.Location = new System.Drawing.Point(315, 25);
+            this.radioButton10.Name = "radioButton10";
+            this.radioButton10.Size = new System.Drawing.Size(58, 16);
+            this.radioButton10.TabIndex = 15;
+            this.radioButton10.TabStop = true;
+            this.radioButton10.Text = "その他";
+            this.radioButton10.UseVisualStyleBackColor = true;
             // 
             // TabFile
             // 
